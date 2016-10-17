2c519b8bf91e0899f7dd412f1dd77f64498c950e
allisonnn <chenxiaoo@outlook.com>
2016-10-17 11:01:25 -0700

    init

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float speed = 1f;
+
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveH = Input.GetAxis ("Horizontal");
+		float moveV = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveH, moveV);
+		rb2d.AddForce (movement * speed);
+	}
+}
